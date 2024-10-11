870d8880e2ee1c88ee723ce33d5b35f9daac13bc
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-10-11 09:46:19 +0200

    Initial commit

---
+++ src/main/java/org/example/UneFenetre.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.awt.*;
+import javax.swing.*;
+
+class UneFenetre extends JFrame {
+    private final int LARG = 500, HAUT = 100; // Hauteur réduite pour une ligne
+    private final int NB_LIGNES = 5;
+
+    public UneFenetre() {
+        super("Le Mobile");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container leconteneur = getContentPane();
+        leconteneur.setLayout(new GridLayout(NB_LIGNES, 1)); // 5 lignes
+
+        for (int i = 0; i < NB_LIGNES; i++) {
+            UnMobile sonMobile = new UnMobile(LARG, HAUT);
+            leconteneur.add(sonMobile);
+            new Thread(sonMobile).start(); // Démarre le thread
+        }
+
+        setSize(LARG, NB_LIGNES * HAUT); // Ajuste la taille de la fenêtre
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new UneFenetre());
+    }
+}
