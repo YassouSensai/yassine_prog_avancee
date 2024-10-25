6226c42a6c1f94ef07b7d794e64bd190aed205d8
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2024-10-25 09:23:37 +0200

    Affichage tp2 avec semaphore binaire

--- src/main/java/tp2/Affichage.java
+++ src/main/java/tp2/Affichage.java
@@ -1,22 +1,43 @@
 package tp2; /**
- * 
+ *
  */
+
+import java.io.*;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.BufferedReader;
 import java.lang.String;
+import java.util.concurrent.Semaphore;
 
-public class Affichage extends Thread{
-	String texte; 
-        
-     static Exclusion exclusionMutuelle = new Exclusion();
+class Exclusion {};
 
-	public Affichage (String txt){texte=txt;}
-	
-	public void run(){
+public class Affichage extends Thread {
+	String texte;
 
-	    synchronized (exclusionMutuelle) { //section critique
-	    for (int i=0; i<texte.length(); i++){
-		    System.out.print(texte.charAt(i));
-		    try {sleep(100);} catch(InterruptedException e){};
+	static Exclusion exclusionMutuelle = new Exclusion();
+
+	static semaphoreBinaire sem = new semaphoreBinaire(1);
+
+	public Affichage(String txt) {
+		texte = txt;
+	}
+
+	public void run() {
+
+		sem.syncWait();
+		System.out.println("J'entre en section critique");
+
+		for (int i = 0; i < texte.length(); i++) {
+			System.out.print(texte.charAt(i));
+
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+			}
 		}
-	    }
+
+		System.out.println("Je sors de section critique");
+		sem.syncSignal();
+
 	}
 }
