801a2e97f4bc4e4b0c42e94c1d3ae51ee000d598
ELKHALKI Yassine <yassine.elkhalki@outlook.fr>
2025-01-27 17:58:59 +0100

    Stockage dans les fichiers

--- src/main/java/tp4/Master.java
+++ src/main/java/tp4/Master.java
@@ -1,7 +1,8 @@
 package tp4;
 
-import tp4.Worker;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
@@ -11,15 +12,13 @@ import java.util.concurrent.*;
  * and aggregates the results.
  */
 class Master {
-    public long doRun(int totalCount, int numWorkers) throws InterruptedException, ExecutionException
-    {
+    public long doRun(int totalCount, int numWorkers, long t1) throws InterruptedException, ExecutionException, IOException {
 
         long startTime = System.currentTimeMillis();
 
         // Collection de tâches
-        List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
-        for (int i = 0; i < numWorkers; ++i)
-        {
+        List<Callable<Long>> tasks = new ArrayList<>();
+        for (int i = 0; i < numWorkers; ++i) {
             tasks.add(new Worker(totalCount));
         }
 
@@ -29,8 +28,7 @@ class Master {
         long total = 0;
 
         // Assemble the results.
-        for (Future<Long> f : results)
-        {
+        for (Future<Long> f : results) {
             // Call to get() is an implicit barrier.  This will block
             // until result from corresponding worker is ready.
             total += f.get();
@@ -38,17 +36,30 @@ class Master {
         double pi = 4.0 * total / totalCount / numWorkers;
 
         long stopTime = System.currentTimeMillis();
+        long duration = stopTime - startTime;
 
-        System.out.println("\nPi : " + pi );
-        System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
+        double sp = (double) t1 / duration;
 
-        System.out.println("Ntot: " + totalCount*numWorkers);
+        System.out.println("\nPi : " + pi);
+        System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) + "\n");
+
+        System.out.println("Ntot: " + totalCount * numWorkers);
         System.out.println("Available processors: " + numWorkers);
-        System.out.println("Time Duration (ms): " + (stopTime - startTime) + "\n");
+        System.out.println("Time Duration (ms): " + duration + "\n");
 
-        System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+        System.out.println((Math.abs((pi - Math.PI)) / Math.PI) + " " + totalCount * numWorkers + " " + numWorkers + " " + duration);
+
+        // Write results to file
+        writeResultsToFile(totalCount, numWorkers, pi, duration, sp);
 
         exec.shutdown();
         return total;
+    }
+
+    private void writeResultsToFile(int totalCount, int numWorkers, double pi, long duration, double sp) throws IOException {
+        String filePath = "src/main/java/tp4/results.csv";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(totalCount * numWorkers + "," + numWorkers + "," + pi + "," + duration + "ms," + sp + "\n");
+        }
     }
 }
